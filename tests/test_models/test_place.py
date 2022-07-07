d68580c6e5b01580ccb7e871cc6c9a8384e16c9e
alejuran <alejandrourn@gmail.com>
2022-07-07 11:25:53 -0500

    test place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,22 +2,35 @@
 """unittest for place"""
 import unittest
 from models.base_model import BaseModel
-from models.place import place
+from models import place
 from models.engine.file_storage import FileStorage
 import os
+Place = place.Place
 
 
-class testPlace(unittest.TestCase):
+class TestPlace(unittest.TestCase):
     """test place class"""
 
     def setUp(self):
         """test Set method"""
         pass
 
-    def test_attr(self):
+    def test_city_id_attr(self):
         """test attributes correct types"""
-        city = Place()
-        self.assertTrue(hasttr(place, "name"))
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertEqual(place.city_id, "")
+
+    def test_user_id_attr(self):
+        """test attributes correct types"""
+        place = Place()
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertEqual(place.user_id, "")
+
+    def test_name_attr(self):
+        """test attributes correct types"""
+        place = Place()
+        self.assertTrue(hasattr(place, "name"))
         self.assertEqual(place.name, "")
 
     def test_str(self):
